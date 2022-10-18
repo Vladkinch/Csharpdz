a11576b24a5d82ffdf6d97181ed302bdffc2bc79
vlad <vlad.kincarov.1994@mail.ru>
2022-10-18 05:34:12 +0400

    if+elseL

---
+++ poexali4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введитеимя пользователя");
+string username = Console.ReadLine();
+ if(username.ToLower() == "саша")                               
+{
+    Console.WriteLine("УРА саша привет");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
+
+// то ловер означает нижний регистр ввода 
