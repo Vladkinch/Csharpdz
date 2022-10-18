38a6c2702044bfa416ad39debdba7d892a64e7b2
vlad <vlad.kincarov.1994@mail.ru>
2022-10-18 04:53:18 +0400

    RANDOMe+L

--- poexali3/Program.cs
+++ poexali3/Program.cs
@@ -1,4 +1,7 @@
-﻿double NumberA = new Random().Next(1,10);
-double NumberB = new Random().Next(1,10);
-Console.WriteLine(NumberA/NumberB);
+﻿int NumberA = new Random().Next(1,10);
+Console.WriteLine (NumberA); 
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+Console.WriteLine(NumberA+NumberB);
+
 
