3cc6f0f5efe44306679084e03b2875572b60bb0e
vlad <vlad.kincarov.1994@mail.ru>
2022-10-19 23:11:24 +0400

    квадрат и трехзначчисла

---
+++ seminar1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Console.WriteLine("введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// Console.WriteLine("число в квадрате: " + sqrt);
+
+Console.Write("введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+{
+    Console.WriteLine("последняя цифра трехзначного числа: " + Math.Abs(number) % 10);
+
+}
+else
+{
+    Console.WriteLine("введено не трех значное число");
+}
